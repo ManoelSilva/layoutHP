a79565ee06cb9f312d550a70d3b8aec37c79d459
ManoelSilva <manoelps.ti@gmail.com>
2017-04-28 00:22:08 -0300

    iniciado repositorio

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+w3IncludeHTML();
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+        $('.to-top').fadeIn();
+    } else {
+        $('.to-top').fadeOut();
+    }
+});
+
+$('.to-top').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
