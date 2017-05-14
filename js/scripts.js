c82b0e4e0c17d0e7574b23c08da5b854d331f81e
ManoelSilva <manoelps.ti@gmail.com>
2017-05-14 12:05:01 -0300

    Identação de alguns códigos

--- js/scripts.js
+++ js/scripts.js
@@ -17,38 +17,38 @@ $('.to-top').click(function () {
 
 /* thumbnail + carousel */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
- /* activate the carousel */
- $("#modal-carousel").carousel({interval:false});
+    /* activate the carousel */
+    $("#modal-carousel").carousel({interval: false});
 
- /* change modal title when slide changes */
- $("#modal-carousel").on("slid.bs.carousel",       function () {
-    $(".modal-title")
-    .html($(this)
-        .find(".active img")
-        .attr("title"));
-});
+    /* change modal title when slide changes */
+    $("#modal-carousel").on("slid.bs.carousel", function () {
+        $(".modal-title")
+            .html($(this)
+                .find(".active img")
+                .attr("title"));
+    });
 
- /* when clicking a thumbnail */
- $(".row .thumbnail").click(function(){
-    var content = $(".carousel-inner");
-    var title = $(".modal-title");
+    /* when clicking a thumbnail */
+    $(".row .thumbnail").click(function () {
+        var content = $(".carousel-inner");
+        var title = $(".modal-title");
 
-    content.empty();
-    title.empty();
+        content.empty();
+        title.empty();
 
-    var id = this.id;
-    var repo = $("#img-repo .item");
-    var repoCopy = repo.filter("#" + id).clone();
-    var active = repoCopy.first();
+        var id = this.id;
+        var repo = $("#img-repo .item");
+        var repoCopy = repo.filter("#" + id).clone();
+        var active = repoCopy.first();
 
-    active.addClass("active");
-    title.html(active.find("img").attr("title"));
-    content.append(repoCopy);
+        active.addClass("active");
+        title.html(active.find("img").attr("title"));
+        content.append(repoCopy);
 
-    // show the modal
-    $("#modal-gallery").modal("show");
-});
+        // show the modal
+        $("#modal-gallery").modal("show");
+    });
 
 });
